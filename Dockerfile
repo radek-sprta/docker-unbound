ad0ba76c0fd3301131fb551bcc6ca8c70229a5d7
Radek SPRTA <mail@radeksprta.eu>
2020-08-22 05:39:07 +0200

    Initial commit

---
+++ Dockerfile
@@ -0,0 +1,22 @@
+# Get OpenNIC root hints
+FROM alpine:latest AS hints
+RUN apk add --update bind-tools && \
+    dig . NS @45.56.115.189 > opennic.root
+
+
+# Main image
+FROM alpine:latest
+MAINTAINER Radek Sprta <mail@radeksprta.eu>
+
+EXPOSE 53
+
+RUN apk add --update unbound
+
+# Configuration
+COPY forward-zones.conf /etc/unbound/forward-zones.conf
+COPY local-zones.conf /etc/unbound/local-zones.conf
+COPY --from=hints opennic.root /usr/share/dns-root-hints/opennic.root
+COPY unbound.conf /etc/unbound/unbound.conf
+
+COPY entrypoint.sh /entrypoint.sh
+CMD ["/entrypoint.sh"]
---
+++ entrypoint.sh
@@ -0,0 +1,31 @@
+#! /bin/sh
+
+CACHE_MIN_TTL=${CACHE_MIN_TTL:-0}
+DO_IPV6=${DO_IPV6:-yes}
+DO_IPV4=${DO_IPV4:-yes}
+DO_UDP=${DO_UDP:-yes}
+DO_TCP=${DO_TCP:-yes}
+RATELIMIT=${RATELIMIT:-0}
+ROOT_HINTS=${ROOT_HINTS:-named}
+SO_REUSEPORT=${SO_REUSEPORT:-yes}
+SERVE_EXPIRED=${SERVE_EXPIRED:-yes}
+STATISTICS_INTERVAL=${STATISTICS_INTERVAL:-0}
+STATISTICS_CUMULATIVE=${STATISTICS_CUMULATIVE:-no}
+EXTENDED_STATISTICS=${EXTENDED_STATISTICS:-no}
+VERBOSITY=${VERBOSITY:-1}
+
+sed 's/{{CACHE_MIN_TTL}}/'"${CACHE_MIN_TTL}"'/' -i /etc/unbound/unbound.conf
+sed 's/{{DO_IPV6}}/'"${DO_IPV6}"'/' -i /etc/unbound/unbound.conf
+sed 's/{{DO_IPV4}}/'"${DO_IPV4}"'/' -i /etc/unbound/unbound.conf
+sed 's/{{DO_UDP}}/'"${DO_UDP}"'/' -i /etc/unbound/unbound.conf
+sed 's/{{DO_TCP}}/'"${DO_TCP}"'/' -i /etc/unbound/unbound.conf
+sed 's/{{RATELIMIT}}/'"${RATELIMIT}"'/' -i /etc/unbound/unbound.conf
+sed 's/{{ROOT_HINTS}}/'"${ROOT_HINTS}"'/' -i /etc/unbound/unbound.conf
+sed 's/{{SO_REUSEPORT}}/'"${SO_REUSEPORT}"'/' -i /etc/unbound/unbound.conf
+sed 's/{{SERVE_EXPIRED}}/'"${SERVE_EXPIRED}"'/' -i /etc/unbound/unbound.conf
+sed 's/{{STATISTICS_INTERVAL}}/'"${STATISTICS_INTERVAL}"'/' -i /etc/unbound/unbound.conf
+sed 's/{{STATISTICS_CUMULATIVE}}/'"${STATISTICS_CUMULATIVE}"'/' -i /etc/unbound/unbound.conf
+sed 's/{{EXTENDED_STATISTICS}}/'"${EXTENDED_STATISTICS}"'/' -i /etc/unbound/unbound.conf
+sed 's/{{VERBOSITY}}/'"${VERBOSITY}"'/' -i /etc/unbound/unbound.conf
+
+exec /usr/sbin/unbound -c /etc/unbound/unbound.conf -d -v
